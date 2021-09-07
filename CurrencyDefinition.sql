b5642c5dee386e573eeb83e54a13414501ee11a5
Balabalaji Sigamsetti <balabalajis@corporate.com>
2021-09-07 17:01:51 +0530

    commiting a SQL file

---
+++ CurrencyDefinition.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[CurrencyDefinition] (
+    [Currency_ID]          VARCHAR (3)  NOT NULL,
+    [Currency_Description] VARCHAR (50) NULL,
+    [Consolidated]         VARCHAR (5)  NULL,
+    [Source_ID]            VARCHAR (5)  NULL,
+    [Source_ID_For_Swap]   VARCHAR (5)  NULL,
+    [IsActive]             VARCHAR (1)  NULL,
+    [CreID]                VARCHAR (25) NULL,
+    [CreTime]              DATETIME     NULL,
+    [ModID]                VARCHAR (25) NULL,
+    [ModTime]              DATETIME     NULL,
+    CONSTRAINT [PK_CurrencyDefinition] PRIMARY KEY CLUSTERED ([Currency_ID] ASC)
+);
+
